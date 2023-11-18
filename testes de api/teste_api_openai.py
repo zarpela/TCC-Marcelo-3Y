8a24a3e18326326f59f7c3bdf36d955d53ef0e9b
Marcelo Zarpelon <marcelinhozarpelon@gmail.com>
2023-11-18 14:06:04 -0300

    deixa os arquivo de python de teste de api numa pasta de testes

---
+++ testes de api/teste_api_openai.py
@@ -0,0 +1,26 @@
+import requests as rq
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+url = "https://api.openai.com/v1/chat/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + SECRET_KEY_GPT
+}
+data = {
+    "model": "gpt-3.5-turbo",
+    "messages": [{"role": "user", "content": "Say this is a test!"}],
+    "temperature": 0.7
+}
+
+response = rq.post(url, headers=headers, data=json.dumps(data))
+
+response_json = response.json()
+
+print(response_json)# to sem credito entao outputa nada, mas em geral é pra tar funcionando
